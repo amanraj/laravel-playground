1617cec9d9a5d71a7c5ae44f93e4be6551947cf4
Aman Raj <rajaman.gaya@gmail.com>
2015-05-13 22:23:57 -0600

    changed team page

--- src/resources/views/footer/team.blade.php
+++ src/resources/views/footer/team.blade.php
@@ -18,7 +18,7 @@ PICKPRIME - Your College Picker
 
 @section('content')
 
-        <div class="container">
+       <div class="container">
                <div class="row">
                <div class="col s12 m12 l6">
                <ul class="collapsible" data-collapsible="accordion">
@@ -26,24 +26,30 @@ PICKPRIME - Your College Picker
                       <div class="collapsible-header" style="height:200px">
                           <table>
                               <tr>
-                                  <td><img  src="http://prime.digitalhollywood.com/Photos/Sheldon_OwenDHS11.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png"></td>
-                                  <td><h5><strong>Sheldon Owen</strong></h5>
-                                  <h6><strong>CEO & Co-Founder</strong></h6></td>
+                                  <td><img  src="candice.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png" ></td>
+                                  <td><h5 class="light-blue-text text-darken-4"><strong>Sheldon Owen</strong></h5>
+                                  <h6><strong>CEO & Co-Founder</strong></h6>
+                                  <img src="fb_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> 
+                                  <img src="linked_in_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> </td>
                               </tr>
-                          </table>    
+                          </table>
+
                       </div>
                       <div class="collapsible-body"><p>Sheldon Owen is the co-founder and CEO of Unified, the creators of the first cloud-based social operating platform for global brands and agencies. </p></div>
                   </li>
 
                   <li>
-                      <div class="collapsible-header" style="height:200px">
+                     <div class="collapsible-header" style="height:200px">
                           <table>
                               <tr>
-                                  <td><img  src="http://prime.digitalhollywood.com/Photos/Sheldon_OwenDHS11.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png"></td>
-                                  <td><h5><strong>Sheldon Owen</strong></h5>
-                                  <h6><strong>CEO & Co-Founder</strong></h6></td>
+                                  <td><img  src="candice.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png" ></td>
+                                  <td><h5 class="light-blue-text text-darken-4"><strong>Sheldon Owen</strong></h5>
+                                  <h6><strong>CEO & Co-Founder</strong></h6>
+                                  <img src="fb_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> 
+                                  <img src="linked_in_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> </td>
                               </tr>
-                          </table>    
+                          </table>
+
                       </div>
                       <div class="collapsible-body"><p>Sheldon Owen is the co-founder and CEO of Unified, the creators of the first cloud-based social operating platform for global brands and agencies. </p></div>
                   </li>
@@ -52,27 +58,33 @@ PICKPRIME - Your College Picker
                 <div class="col s12 m12 l6">
                 <ul class="collapsible" data-collapsible="accordion">
                   <li>
-                      <div class="collapsible-header"  style="height:200px">
+                     <div class="collapsible-header" style="height:200px">
                           <table>
                               <tr>
-                                  <td><img  src="http://prime.digitalhollywood.com/Photos/Sheldon_OwenDHS11.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png"></td>
-                                  <td><h5><strong>Sheldon Owen</strong></h5>
-                                  <h6><strong>CEO & Co-Founder</strong></h6></td>
+                                  <td><img  src="candice.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png" ></td>
+                                  <td><h5 class="light-blue-text text-darken-4"><strong>Sheldon Owen</strong></h5>
+                                  <h6><strong>CEO & Co-Founder</strong></h6>
+                                  <img src="fb_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> 
+                                  <img src="linked_in_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> </td>
                               </tr>
-                          </table>    
+                          </table>
+
                       </div>
                       <div class="collapsible-body"><p>Sheldon Owen is the co-founder and CEO of Unified, the creators of the first cloud-based social operating platform for global brands and agencies. </p></div>
                   </li>
 
                   <li>
-                      <div class="collapsible-header"  style="height:200px">
+                    <div class="collapsible-header" style="height:200px">
                           <table>
                               <tr>
-                                  <td><img  src="http://prime.digitalhollywood.com/Photos/Sheldon_OwenDHS11.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png"></td>
-                                  <td><h5><strong>Sheldon Owen</strong></h5>
-                                  <h6><strong>CEO & Co-Founder</strong></h6></td>
+                                  <td><img  src="candice.jpg" alt="http://www.priorlakeassociation.org/wp-content/uploads/2011/06/blank-profile.png" ></td>
+                                  <td><h5 class="light-blue-text text-darken-4"><strong>Sheldon Owen</strong></h5>
+                                  <h6><strong>CEO & Co-Founder</strong></h6>
+                                  <img src="fb_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> 
+                                  <img src="linked_in_icon.png" href="s#!" class="responsive-img" style="height:30px;width:30px"> </td>
                               </tr>
-                          </table>    
+                          </table>
+
                       </div>
                       <div class="collapsible-body"><p>Sheldon Owen is the co-founder and CEO of Unified, the creators of the first cloud-based social operating platform for global brands and agencies. </p></div>
                   </li>                  
